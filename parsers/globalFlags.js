069183aab59354d8f96c8b004ee174158585ebe7
Nisha Wolfe <contact@nishawolfe.com>
2021-10-07 14:15:34 -0400

    added remainder of ch1 flags

--- parsers/globalFlags.js
+++ parsers/globalFlags.js
@@ -286,7 +286,7 @@ function ParseNewhomeCh1(){
   
   flagArray[262] = {
     title: `Asgore Flower Cutscene`,
-    description: `Whether you've claimed the free hot chocolate at the Diner or not`,
+    description: `Whether you've seen the Asgore cutscene or not`,
     dropdown:[
       {
         "id": 0,
@@ -460,11 +460,305 @@ function ParseNewhomeCh1(){
   
 }
 
+
+function ParseOtherCh1(){
+  
+  flagArray[241] = {
+    title: `Jevil Quest`,
+    description: `The quest flag for unlocking, fighting, and defeating Jevil.`,
+    range:{
+      min:0,
+      max:7
+    }
+  }
+  
+  flagArray[252] = {
+    title: `Bed Inspector`,
+    description: `Whether you've inspected all of the beds in the castle or not.`,
+    toggle:{
+      off: 0,
+      on: 1
+    }
+  }
+  
+  flagArray[253] = {
+    title: `Spin Cake`,
+    description: `Whether you've recieved the Spin Cake or not.`,
+    toggle:{
+      off: 0,
+      on: 1
+    }
+  }
+  
+  flagArray[254] = {
+    title: `The Original Starwalker`,
+    description: `Whether you've spoken to The Original Starwalker or not.`,
+    toggle:{
+      off: 0,
+      on: 1
+    }
+  }
+  
+  flagArray[910] = {
+    title: `Chapter 1 Secret Room`,
+    description: `The state of interaction with the secret room in Chapter 1`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "Haven't Seen",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "Seen Room",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "Recieved Egg",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[911] = {
+    title: `Chapter 1 Eggs`,
+    description: `The number of eggs you've gotten in Chapter 1`,
+    range:{
+      min:-9999999999,
+      max:9999999999
+    },
+  }
+  
+}
+
+function ParseGoner(){
+  flagArray[900] = {
+    title: `Vessel Head`,
+    description: `The Head for the Vessel`,
+    range:{
+      min:0,
+      max:7
+    }
+  }
+  
+  flagArray[901] = {
+    title: `Vessel Body`,
+    description: `The Body for the Vessel`,
+    range:{
+      min:0,
+      max:5
+    }
+  }
+  
+  flagArray[902] = {
+    title: `Vessel Legs`,
+    description: `The Legs for the Vessel`,
+    range:{
+      min:0,
+      max:4
+    }
+  }
+  
+  flagArray[903] = {
+    title: `What is its favorite food?`,
+    description: `What is its favorite food?`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "SWEET",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "SOFT",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "SOUR",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "SALTY",
+        "description": ""
+      },
+      {
+        "id": 4,
+        "title": "PAIN",
+        "description": ""
+      },
+      {
+        "id": 5,
+        "title": "COLD",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[904] = {
+    title: `Your favorite blood type?`,
+    description: `Your favorite blood type?`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "A",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "AB",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "B",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "C",
+        "description": ""
+      },
+      {
+        "id": 4,
+        "title": "D",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[905] = {
+    title: `What color does it like most?`,
+    description: `What color does it like most?`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "RED",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "BLUE",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "GREEN",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "CYAN",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[906] = {
+    title: `How do you feel about your creation? (it will not hear.)`,
+    description: `How do you feel about your creation? (it will not hear.)`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "LOVE",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "HOPE",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "DISGUST",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "FEAR",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[907] = {
+    title: `Have you answered honestly?`,
+    description: `Have you answered honestly?`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "YES",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "NO",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[908] = {
+    title: `You acknowledge the possibility of pain and seizure.`,
+    description: `You acknowledge the possibility of pain and seizure.`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "YES",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "NO",
+        "description": ""
+      }
+    ]
+  }
+  
+  flagArray[909] = {
+    title: `Please give it a gift.`,
+    description: `Please give it a gift.`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "KINDNESS",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "MIND",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "AMBITION",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "BRAVERY",
+        "description": ""
+      },
+      {
+        "id": 4,
+        "title": "VOICE",
+        "description": ""
+      }
+    ]
+  }
+  
+}
+
+
 function ParseGlobalFlags(){
   return new Promise((resolve,reject)=>{
     ParseFlags();
     ParseThrashMachine();
     ParseNewhomeCh1();
+    ParseOtherCh1();
+    ParseGoner();
     
     SetFlags();
     
