21fe66e8beefd92f60e8c7e8b7869cafc059c7f2
Nisha Wolfe <contact@nishawolfe.com>
2021-10-06 20:06:24 -0400

    added global flags

--- parsers/globalFlags.js
+++ parsers/globalFlags.js
@@ -14,8 +14,10 @@ function SetFlags(){
       }
     }else{
       flagArray[i].var = `flag[${i}]`;
+      if(flagArray[i].description == null){
+        flagArray[i].description = ``;
+      }
     }
-
   }
 }
 
@@ -23,32 +25,167 @@ function ParseFlags(){
   
   flagArray[11] = {
     title: `Auto Run`,
-    description: ``,
     toggle:{
       off: 0,
       on: 1
-    },
+    }
   }
   
+  flagArray[34] = {
+    title: `Party X Action Learned`,
+    description: `Whether your party members can ACT`,
+    toggle:{
+      off: 0,
+      on: 1
+    }
+  }
+}
+
+function ParseThrashMachine(){
+  flagArray[220] = {
+    title: `Thrash Machine Head`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "Laser",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "Sword",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "Flame",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "Duck",
+        "description": ""
+      }
+    ]
+  }
   
+  flagArray[221] = {
+    title: `Thrash Machine Body`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "Plain",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "Wheel",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "Tank",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "Duck",
+        "description": ""
+      }
+    ]
+  }
   
+  flagArray[222] = {
+    title: `Thrash Machine Feet`,
+    dropdown:[
+      {
+        "id": 0,
+        "title": "Sneak",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "A.whl",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "Tread",
+        "description": ""
+      },
+      {
+        "id": 3,
+        "title": "Duck",
+        "description": ""
+      }
+    ]
+  }
   
+  flagArray[223] = {
+    title: `Thrash Machine Head Color`,
+    range:{
+      min:0,
+      max:31
+    }
+  }
   
+  flagArray[224] = {
+    title: `Thrash Machine Body Color`,
+    range:{
+      min:0,
+      max:31
+    }
+  }
   
+  flagArray[225] = {
+    title: `Thrash Machine Feet Color`,
+    range:{
+      min:0,
+      max:31
+    }
+  }
+}
+
+function ParseNewhomeCh1(){
   
+  flagArray[255] = {
+    title: `Noelle Hospital Cutscene`,
+    description: `The cutscene that plays at the end of Chapter 1 when you visit the Hospital for the first time.`
+    dropdown:[
+      {
+        "id": 0,
+        "title": "Haven't Watched",
+        "description": ""
+      },
+      {
+        "id": 1,
+        "title": "Watched",
+        "description": ""
+      },
+      {
+        "id": 2,
+        "title": "Talked to Rudy",
+        "description": ""
+      }
+    ]
+  }
   
-  
-  
-  
-  
-  
+  flagArray[256] = {
+    title: `Talked to Berdly`,
+    description: `Whether you've talked to Berdly or not`,
+    toggle:{
+      off: 0,
+      on: 1
+    }
+  }
 }
 
 function ParseGlobalFlags(){
   return new Promise((resolve,reject)=>{
     ParseFlags();
-    SetFlags();
+    ParseThrashMachine();
+    ParseNewhomeCh1();
     
+    SetFlags();
     
     resolve(flagArray);
   });
